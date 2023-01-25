e475ba5a394f47465cbbd5d6aed0f2d0b50d976d
Jason <jajones414@gmail.com>
2023-01-24 20:46:30 -0600

    Added portfolio cards to Portfolio.js.

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,12 @@
 import "../css/App.css";
 import Header from "./components/Header";
-
+import Portfolio from "./components/Portfolio";
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
        <Header /> 
+       <Portfolio />
       </header>
     </div>
   );
---
+++ src/js/components/Portfolio.js
@@ -0,0 +1,147 @@
+import React from "react";
+
+export default function Portfolio() {
+    
+return(
+       <div id="portCards">
+<div className="row row-cols-1 row-cols-md-3 g-4">
+  <div id="singleProj"className="col">
+    <div className="card">
+      {/* <img src="..." className="card-img-top" alt="..."> */}
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h6>Skills used:</h6>
+      <button id="portfolioBtns">View Project</button>
+      </div>
+    </div>
+  </div>
+  <div id="singleProj" className="col">
+    <div className="card">
+      {/* <img src="..." className="card-img-top" alt="..."> */}
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h6>Skills used:</h6>
+        <button id="portfolioBtns">View Project</button>
+      </div>
+    </div>
+  </div>
+  <div id="singleProj" className="col">
+    <div class="card">
+      {/* <img src="..." class="card-img-top" alt="..."> */}
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        <h6>Skills used:</h6>
+        <button id="portfolioBtns">View Project</button>
+      </div>
+    </div>
+  </div>
+  <div id="singleProj" class="col">
+    <div class="card">
+      {/* <img src="..." class="card-img-top" alt="..."> */}
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h6>Skills used:</h6>
+        <button id="portfolioBtns">View Project</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div className="row row-cols-1 row-cols-md-3 g-4">
+  <div id="singleProj" className="col">
+    <div className="card">
+      {/* <img src="..." className="card-img-top" alt="..."> */}
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h6>Skills used:</h6>
+      <button id="portfolioBtns">View Project</button>
+      </div>
+    </div>
+  </div>
+  <div id="singleProj" className="col">
+    <div className="card">
+      {/* <img src="..." className="card-img-top" alt="..."> */}
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h6>Skills used:</h6>
+        <button id="portfolioBtns">View Project</button>
+      </div>
+    </div>
+  </div>
+  <div id="singleProj" className="col">
+    <div class="card">
+      {/* <img src="..." class="card-img-top" alt="..."> */}
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        <h6>Skills used:</h6>
+        <button id="portfolioBtns">View Project</button>
+      </div>
+    </div>
+  </div>
+  <div id="singleProj" class="col">
+    <div class="card">
+      {/* <img src="..." class="card-img-top" alt="..."> */}
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h6>Skills used:</h6>
+        <button id="portfolioBtns">View Project</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div className="row row-cols-1 row-cols-md-3 g-4">
+  <div id="singleProj" className="col">
+    <div className="card">
+      {/* <img src="..." className="card-img-top" alt="..."> */}
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h6>Skills used:</h6>
+      <button id="portfolioBtns">View Project</button>
+      </div>
+    </div>
+  </div>
+  <div id="singleProj" className="col">
+    <div className="card">
+      {/* <img src="..." className="card-img-top" alt="..."> */}
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h6>Skills used:</h6>
+        <button id="portfolioBtns">View Project</button>
+      </div>
+    </div>
+  </div>
+  <div id="singleProj" className="col">
+    <div class="card">
+      {/* <img src="..." class="card-img-top" alt="..."> */}
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        <h6>Skills used:</h6>
+        <button id="portfolioBtns">View Project</button>
+      </div>
+    </div>
+  </div>
+  <div id="singleProj" class="col">
+    <div class="card">
+      {/* <img src="..." class="card-img-top" alt="..."> */}
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h6>Skills used:</h6>
+        <button id="portfolioBtns">View Project</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+)
+}
