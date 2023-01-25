82283f87f38b9ded5f6507cef098ffb8ec544c9e
Jason <jajones414@gmail.com>
2023-01-25 16:50:43 -0600

    Added data to portfolio cards.

--- src/js/components/Portfolio.js
+++ src/js/components/Portfolio.js
@@ -1,146 +1,172 @@
 import React from "react";
-
+import moonstone from '../../images/moonstone.jpg'
+import socialApi from '../../images/socialMediaApi.jpg'
 export default function Portfolio() {
-    
-return(
-       <div id="portCards">
-<div className="row row-cols-1 row-cols-md-3 g-4">
-  <div id="singleProj"className="col">
-    <div className="card">
-      {/* <img src="..." className="card-img-top" alt="..."> */}
-      <div className="card-body">
-        <h5 className="card-title">laurenatmoonstone.com</h5>
-        <h6>Skills used:</h6>
-      <button href="https://laurenatmoonstone.com/" id="portfolioBtns">View Project</button>
+  return (
+    <div id="portCards">
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div id="singleProj" className="col">
+          <div className="card">
+            <img src={moonstone} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">laurenatmoonstone.com</h5>
+              <h6>Skills used:</h6>
+              <a href="https://laurenatmoonstone.com/" id="portfolioBtns">
+                View Project
+              </a>
+              <br></br>
+              <br />
+            </div>
+          </div>
+        </div>
+        <div id="singleProj" className="col">
+          <div className="card">
+            {/* <img src="..." className="card-img-top" alt="..."> */}
+            <div className="card-body">
+              <h5 className="card-title">ffffff</h5>
+
+              <h6>Skills used:</h6>
+              <a id="portfolioBtns">View Project</a>
+              <br></br>
+              <br />
+            </div>
+          </div>
+        </div>
+        <div id="singleProj" className="col">
+          <div class="card">
+            {/* <img src="..." class="card-img-top" alt="..."> */}
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+
+              <h6>Skills used:</h6>
+              <a id="portfolioBtns">View Project</a>
+              <br></br>
+              <br />
+            </div>
+          </div>
+        </div>
+        <div id="singleProj" class="col">
+          <div class="card">
+            {/* <img src="..." class="card-img-top" alt="..."> */}
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+
+              <h6>Skills used:</h6>
+              <a id="portfolioBtns">View Project</a>
+              <br></br>
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div id="singleProj" className="col">
+          <div className="card">
+            <img src={socialApi} className="card-img-top" alt="..." width="100" />
+            <div className="card-body">
+              <h5 className="card-title">Social Media Back-End API</h5>
+
+              <h6>Skills used:</h6>
+              <a id="portfolioBtns">View Project</a>
+              <br></br>
+              <br />
+            </div>
+          </div>
+        </div>
+        <div id="singleProj" className="col">
+          <div className="card">
+            {/* <img src="..." className="card-img-top" alt="..."> */}
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+
+              <h6>Skills used:</h6>
+              <a id="portfolioBtns">View Project</a>
+              <br></br>
+              <br />
+            </div>
+          </div>
+        </div>
+        <div id="singleProj" className="col">
+          <div class="card">
+            {/* <img src="..." class="card-img-top" alt="..."> */}
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+
+              <h6>Skills used:</h6>
+              <a id="portfolioBtns">View Project</a>
+              <br></br>
+              <br />
+            </div>
+          </div>
+        </div>
+        <div id="singleProj" class="col">
+          <div class="card">
+            {/* <img src="..." class="card-img-top" alt="..."> */}
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+
+              <h6>Skills used:</h6>
+              <a id="portfolioBtns">View Project</a>
+              <br></br>
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div id="singleProj" className="col">
+          <div className="card">
+            {/* <img src="..." className="card-img-top" alt="..."> */}
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+
+              <h6>Skills used:</h6>
+              <a id="portfolioBtns">View Project</a>
+              <br></br>
+              <br />
+            </div>
+          </div>
+        </div>
+        <div id="singleProj" className="col">
+          <div className="card">
+            {/* <img src="..." className="card-img-top" alt="..."> */}
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+
+              <h6>Skills used:</h6>
+              <a id="portfolioBtns">View Project</a>
+              <br></br>
+              <br />
+            </div>
+          </div>
+        </div>
+        <div id="singleProj" className="col">
+          <div class="card">
+            {/* <img src="..." class="card-img-top" alt="..."> */}
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+
+              <h6>Skills used:</h6>
+              <a id="portfolioBtns">View Project</a>
+              <br></br>
+              <br />
+            </div>
+          </div>
+        </div>
+        <div id="singleProj" class="col">
+          <div class="card">
+            {/* <img src="..." class="card-img-top" alt="..."> */}
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+
+              <h6>Skills used:</h6>
+              <a id="portfolioBtns">View Project</a>
+              <br></br>
+              <br />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <div id="singleProj" className="col">
-    <div className="card">
-      {/* <img src="..." className="card-img-top" alt="..."> */}
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        
-        <h6>Skills used:</h6>
-        <button id="portfolioBtns">View Project</button>
-      </div>
-    </div>
-  </div>
-  <div id="singleProj" className="col">
-    <div class="card">
-      {/* <img src="..." class="card-img-top" alt="..."> */}
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-       
-        <h6>Skills used:</h6>
-        <button id="portfolioBtns">View Project</button>
-      </div>
-    </div>
-  </div>
-  <div id="singleProj" class="col">
-    <div class="card">
-      {/* <img src="..." class="card-img-top" alt="..."> */}
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        
-        <h6>Skills used:</h6>
-        <button id="portfolioBtns">View Project</button>
-      </div>
-    </div>
-  </div>
-</div>
-<div className="row row-cols-1 row-cols-md-3 g-4">
-  <div id="singleProj" className="col">
-    <div className="card">
-      {/* <img src="..." className="card-img-top" alt="..."> */}
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        
-        <h6>Skills used:</h6>
-      <button id="portfolioBtns">View Project</button>
-      </div>
-    </div>
-  </div>
-  <div id="singleProj" className="col">
-    <div className="card">
-      {/* <img src="..." className="card-img-top" alt="..."> */}
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        
-        <h6>Skills used:</h6>
-        <button id="portfolioBtns">View Project</button>
-      </div>
-    </div>
-  </div>
-  <div id="singleProj" className="col">
-    <div class="card">
-      {/* <img src="..." class="card-img-top" alt="..."> */}
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-       
-        <h6>Skills used:</h6>
-        <button id="portfolioBtns">View Project</button>
-      </div>
-    </div>
-  </div>
-  <div id="singleProj" class="col">
-    <div class="card">
-      {/* <img src="..." class="card-img-top" alt="..."> */}
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        
-        <h6>Skills used:</h6>
-        <button id="portfolioBtns">View Project</button>
-      </div>
-    </div>
-  </div>
-</div>
-<div className="row row-cols-1 row-cols-md-3 g-4">
-  <div id="singleProj" className="col">
-    <div className="card">
-      {/* <img src="..." className="card-img-top" alt="..."> */}
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        
-        <h6>Skills used:</h6>
-      <button id="portfolioBtns">View Project</button>
-      </div>
-    </div>
-  </div>
-  <div id="singleProj" className="col">
-    <div className="card">
-      {/* <img src="..." className="card-img-top" alt="..."> */}
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        
-        <h6>Skills used:</h6>
-        <button id="portfolioBtns">View Project</button>
-      </div>
-    </div>
-  </div>
-  <div id="singleProj" className="col">
-    <div class="card">
-      {/* <img src="..." class="card-img-top" alt="..."> */}
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-       
-        <h6>Skills used:</h6>
-        <button id="portfolioBtns">View Project</button>
-      </div>
-    </div>
-  </div>
-  <div id="singleProj" class="col">
-    <div class="card">
-      {/* <img src="..." class="card-img-top" alt="..."> */}
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        
-        <h6>Skills used:</h6>
-        <button id="portfolioBtns">View Project</button>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-)
+  );
 }
--- src/js/components/Resume.js
+++ src/js/components/Resume.js
@@ -1,25 +1,25 @@
 import React from "react";
 import "../../css/App.css";
 export default function Resume() {
-
   return (
-    <div >
+    <div>
       <div id="resume" className="card">
-        <div className="reumeCard">
-                <h1>Resume</h1>
-                <h3>
-                  Thank you for taking interest in my portfolio, please feel
-                  free to download my resume and reach out to me with any
-                  questions!
-                </h3>
-                  <a
-                    id="resumeBtn"
-                    href="https://drive.google.com/file/d/1h_8IlL0DU9sexRt6aMdwtbYSm4DyQ4ce/view?usp=sharing"
-                  >
-                    View Resume
-                  </a>
-                </div>
-              </div>
-            </div>
+        <div className="">
+          <h1>Resume</h1>
+          <h3 id="resumeText">
+            Thank you for taking interest in my portfolio, please feel free to
+            download my resume and reach out to me with any questions!
+          </h3>
+        </div>
+        <a
+          id="resumeBtn"
+          href="https://drive.google.com/file/d/1h_8IlL0DU9sexRt6aMdwtbYSm4DyQ4ce/view?usp=sharing"
+        >
+          View Resume
+        </a>
+        <br></br>
+        <br />
+      </div>
+    </div>
   );
 }
