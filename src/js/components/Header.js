5af4b67cf1b8b7043cb8e31b62ab6546e752f424
Jason <jajones414@gmail.com>
2023-01-24 22:25:21 -0600

    Updated Header.js.

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,9 +1,25 @@
 import React from "react";
+import '../../css/App.css'
 
 export default function Header() {
     return (
-        <>
-        </>
+        <div id="headerStyle">
+        <h1 id="maName">Jason Jones</h1>
+     <div id="navigate">
+     <div>
+        <a id="navigate" href="">About me</a>
+     </div>
+     <div>
+        <a id="navigate" href="">Portfolio</a>
+     </div>
+     <div>
+        <a id="navigate" href="">Contact</a>
+     </div>
+     <div>
+        <a id="navigate" href="">Resume</a>
+     </div>
+</div>
+        </div>
     );
   }
   
