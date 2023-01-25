123dd14e0e0067ab0ad054f9b8dde0c1091b6fe1
Jason <jajones414@gmail.com>
2023-01-25 15:39:51 -0600

    Updated resume button.

--- src/js/components/Resume.js
+++ src/js/components/Resume.js
@@ -5,23 +5,21 @@ export default function Resume() {
   return (
     <div >
       <div id="resume" className="card">
-        <div className="">
+        <div className="reumeCard">
                 <h1>Resume</h1>
                 <h3>
                   Thank you for taking interest in my portfolio, please feel
                   free to download my resume and reach out to me with any
                   questions!
                 </h3>
-                <div classNameName="btnDiv">
-                  <button
-                    id="downloadBtn"
+                  <a
+                    id="resumeBtn"
                     href="https://drive.google.com/file/d/1h_8IlL0DU9sexRt6aMdwtbYSm4DyQ4ce/view?usp=sharing"
                   >
                     View Resume
-                  </button>
+                  </a>
                 </div>
               </div>
             </div>
-          </div>
   );
 }
