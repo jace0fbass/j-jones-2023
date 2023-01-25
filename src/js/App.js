ce8744d9ec45b8ea6704abf9b6a4814619a51bea
Jason <jajones414@gmail.com>
2023-01-24 18:11:31 -0600

    Organized files and folders and added Header.js data.

---
+++ src/js/App.js
@@ -0,0 +1,12 @@
+import "../css/App.css";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Header /> 
+      </header>
+    </div>
+  );
+}
