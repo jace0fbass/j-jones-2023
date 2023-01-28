7bc69992fa1cc026b900ef073a6f7d4577889247
Jason <jajones414@gmail.com>
2023-01-27 22:27:28 -0600

    Updated resume mobile styling.

--- src/js/components/Resume.js
+++ src/js/components/Resume.js
@@ -2,15 +2,12 @@ import React from "react";
 import "../../css/App.css";
 export default function Resume() {
   return (
-    <div>
-      <div id="resume" className="card">
-        <div className="">
+      <div id="resume-card" className="card">
           <h1>Resume</h1>
           <h3 id="resumeText">
             Thank you for taking interest in my portfolio, please feel free to
             download my resume and reach out to me with any questions!
           </h3>
-        </div>
         <a
           id="resumeBtn"
           href="https://drive.google.com/file/d/1h_8IlL0DU9sexRt6aMdwtbYSm4DyQ4ce/view?usp=sharing"
@@ -20,6 +17,5 @@ export default function Resume() {
         <br></br>
         <br />
       </div>
-    </div>
   );
 }
