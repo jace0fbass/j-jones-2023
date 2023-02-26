2810b1cdc0d0b705d7d8163745cd2fa6b30fe9f3
Jason <jajones414@gmail.com>
2023-02-25 22:00:02 -0600

    Updated resume mobile styling.

--- src/js/components/Resume.js
+++ src/js/components/Resume.js
@@ -2,22 +2,56 @@ import React from "react";
 import "../../css/App.css";
 export default function Resume() {
   return (
-      <div id="resume-card" className="card">
-          <h1>Resume</h1>
-          <h3 id="resumeText">
-            Thank you for taking interest in my portfolio, please feel free to
-            download my resume and reach out to me with any questions!
-          </h3>
-        <h2>Skills:</h2>
-        <h3 id="skills" >HTML, CSS, Javascript, jQuery, React, Node, MongoDB, Mongoose ODM, Express, MySQL, Sequelize, RESTful API, OOP, TDD, ORM, MVC, Inquirer, Handlebars, Bootstrap, AJAX, Moment, Git, wxWidgets. </h3>
-        <a
-          id="resumeBtn"
-          href="https://drive.google.com/file/d/1zOIL7bXFdPRRc1L-JsjBpvSemujQKqit/view?usp=share_link"
-        >
-          View Resume
-        </a>
-        <br></br>
-        <br />
-      </div>
+    <div id="projCol" className=" card ">
+      <h3 id="resumeText">
+        Thank you for taking interest in my portfolio, please feel free to
+        download my resume and reach out to me with any questions!
+      </h3>
+      <br></br>
+      <a
+        id="resumeBtn"
+        href="https://drive.google.com/file/d/1zOIL7bXFdPRRc1L-JsjBpvSemujQKqit/view?usp=share_link"
+      >
+        View Resume
+      </a>
+      <br></br>
+      <br></br>
+      <h2 id="resSkills">Skills:</h2>
+      <h3 id="skills">
+        <div id="skillsContainer">
+          <div>
+            <p>HTML5</p>
+            <p>CSS3</p>
+            <p>Javascript</p>
+
+            <p>jQuery</p>
+            <p>Node.js</p>
+            <p>MongoDB</p>
+            <p>Mongoose</p>
+            
+            <p>MySQL</p>
+            <p>Sequelize</p>
+            <p>ORM</p>
+            <p>Apollo</p>
+            <p>Git</p>
+          </div>
+          <div>
+            <p>ReactJS</p>
+            <p>RESTful API</p>
+            <p>GraphQL</p>
+            
+            <p>MVC</p>
+            <p>Express</p>
+            <p>Inquirer</p>
+            <p>OOP</p>
+            <p>Handlebars</p>
+            <p>Bootstrap</p>
+            <p>TDD</p>
+            <p>AJAX</p>
+            
+          </div>
+        </div>
+      </h3>
+    </div>
   );
 }
